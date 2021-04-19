75b2fcff9dc8db3cb814bf5a1aa81a01801c022e
Marco <campos.marcoa@yahoo.com.br>
2021-04-19 17:20:15 -0300

    adicionado cache retrofit

--- app/src/main/java/com/example/hotmartapp/ui/main/Modules.kt
+++ app/src/main/java/com/example/hotmartapp/ui/main/Modules.kt
@@ -1,9 +1,15 @@
 package com.example.hotmartapp.ui.main
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import com.example.hotmartapp.data.repository.*
 import com.example.hotmartapp.source.remote.Api
 import com.example.hotmartapp.source.remote.ApiImage
 import com.example.hotmartapp.ui.details.DetailsViewModel
+import okhttp3.Cache
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -84,12 +90,66 @@ val imageRepositoryModule = module {
 
 //
 
+val ctx = MyApp.applicationContext()
+
+val cacheSize = (5 * 1024 * 1024).toLong()
+val myCache = Cache( ctx.cacheDir, cacheSize)
+
+fun hasNetwork(context: Context): Boolean? {
+    var isConnected: Boolean? = false // Initial Value
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+    if (activeNetwork != null && activeNetwork.isConnected)
+        isConnected = true
+    return isConnected
+}
+
+
+fun provideOfflineCacheInterceptor(context: Context): Interceptor {
+    return Interceptor { chain ->
+        var request = chain.request()
+        var cacheHeaderValue = if (!hasNetwork(context)!!){
+            "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 1
+        } else {
+            "public, max-age=" + 5
+        }
+        request = request.newBuilder().header("Cache-Control", cacheHeaderValue).build()
+        chain.proceed(request)
+    }
+}
+
+fun provideCacheInterceptor(context: Context): Interceptor {
+    return Interceptor { chain ->
+        val request = chain.request()
+        var cacheHeaderValue = if (!hasNetwork(context)!!){
+            "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 1
+        } else {
+            "public, max-age=" + 5
+        }
+        //request = request.newBuilder().build()
+        val response = chain.proceed(request)
+        response.newBuilder()
+                .removeHeader("Pragma")
+                .removeHeader("Cache-Control")
+                .header("Cache-Control", cacheHeaderValue)
+                .build()
+    }
+}
+
+
 fun networkModule() = module {
+    val okHttpClient = OkHttpClient.Builder()
+            .cache(myCache)
+            .addInterceptor(provideOfflineCacheInterceptor(ctx))
+            .addNetworkInterceptor(provideCacheInterceptor(ctx))
+            .build()
+
     val BASE_URL = "https://hotmart-mobile-app.herokuapp.com/"
 
     fun providesApi(): Api {
         val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
@@ -101,11 +161,18 @@ fun networkModule() = module {
 }
 
 fun networkImageModule() = module {
+    val okHttpClient = OkHttpClient.Builder()
+            .cache(myCache)
+            .addInterceptor(provideOfflineCacheInterceptor(ctx))
+            .addNetworkInterceptor(provideCacheInterceptor(ctx))
+            .build()
+
     val BASE_URL = "https://pixabay.com/api/"
 
     fun providesApi(): ApiImage {
         val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
--- app/src/main/java/com/example/hotmartapp/ui/main/MyApp.kt
+++ app/src/main/java/com/example/hotmartapp/ui/main/MyApp.kt
@@ -1,10 +1,24 @@
 package com.example.hotmartapp.ui.main
 
 import android.app.Application
+import android.content.Context
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class MyApp: Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MyApp? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
 
